4f9349ba3bbedb8b87790759531987ef1c3c5615
blackberrypineapple <blackberrypineapple@protonmail.com>
2025-01-02 07:09:14 +0000

    remove crash capture and new error report

--- app/src/main/java/mattecarra/accapp/MainApplication.kt
+++ app/src/main/java/mattecarra/accapp/MainApplication.kt
@@ -37,36 +37,4 @@ class MainApplication: MultiDexApplication()
         mDEBUG = (getDefaultSharedPreferences(applicationContext).getString("appdebug", "0") ?: "0").toInt()
         LogExt().s(javaClass.simpleName, "DEBUG=$mDEBUG " +when(mDEBUG) {0->"[NONE]" 1->"[CONSOLE]" 2->"[FILE]" else->"[UNKNOWN]"})
     }
-
-    override fun attachBaseContext(base: Context?) {
-        super.attachBaseContext(base)
-//        if (!BuildConfig.DEBUG) {
-            init(
-                this, CoreConfigurationBuilder() //core configuration:
-                    .withBuildConfigClass(BuildConfig::class.java)
-                    .withReportFormat(StringFormat.JSON)
-                    .withPluginConfigurations( //each plugin you chose above can be configured with its builder like this:
-                        ToastConfigurationBuilder()
-                            .withText("AccA crashed. Ein Bericht wurde an Markus verschickt.")
-                            .withLength(Toast.LENGTH_LONG)
-                            .build(),
-                        HttpSenderConfigurationBuilder() //required. Https recommended
-                            .withUri("https://acra.bubendorf.net/report") //optional. Enables http basic auth
-                            .withBasicAuthLogin("KHTsEzvtHFxh8czn") //required if above set
-                            .withBasicAuthPassword("Kh7EOjGiMihWzHBE") // defaults to POST
-                            .withHttpMethod(HttpSender.Method.POST) //defaults to 5000ms
-                            .withConnectionTimeout(5000) //defaults to 20000ms
-                            .withSocketTimeout(20000) // defaults to false
-                            .withDropReportsOnTimeout(false) //defaults to false. Recommended if your backend supports it
-                            .withCompress(true) //defaults to all
-                            .withTlsProtocols(TLS.V1_3, TLS.V1_2)
-                            .build(),
-                        LimiterConfigurationBuilder()
-                            .withDeleteReportsOnAppUpdate(true)
-                            .withEnabled(true)
-                            .build()
-                    )
-            )
-//        }
-    }
 }
